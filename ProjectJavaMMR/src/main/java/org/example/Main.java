b443a7555e05a051b3d6bc067afb4003bf8147dd
Mariomonteroramirez <maro2000jv@gmail.com>
2023-03-01 10:33:56 -0600

    Ejecercicio 2

--- ProjectJavaMMR/src/main/java/org/example/Main.java
+++ ProjectJavaMMR/src/main/java/org/example/Main.java
@@ -4,142 +4,39 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       /* System.out.println("Hello world!");
-        System.out.println("Mario Montero Ramirez");
-        int miEntero = 10;
-        float miPuntoFlotante = 0.5f;
-        double miDecimal = 22.5;
-        String miTexto = "Hola soy un texto en Java";
-        char miCaracter = 'a';
 
+    /*if(edad > 18 && edad <= 69  "Los simbolos de &&se utilizan como and"){
+          //se ejecuta una operacion si se cumple la condicion
+     }else{
+       //De lo contrario manda lo que este en else   System.out.println("Error");
+     }*/
 
-        Scanner scanner = new Scanner(System.in);
-
-        int numero;
-        numero = scanner.nextInt();
-        System.out.println("Mi numero que acabo de ingresar es:"+ numero);*/
-
-        //Desarrollar un Programa Java que lea dos numeros los sume y me muestre elresultado
-        /*Scanner scanner = new Scanner(System.in);
-
-        int numero;
-        int numero2;
-
-        System.out.println("Ingrese el numero 1");
-        numero = scanner.nextInt();
-        System.out.println("ingrese el numero 2");
-        numero2 = scanner.nextInt();
-        int Suma = numero + numero2;
-        System.out.println("La suma de los dos numeros es:"+ Suma);*/
-
-
-        //nombre, edad , peso , altura y calcular imc
-        // imc peso/ altura x altura
-
-       /* int miEntero = 10;
-        float miPuntoFlotante = 0.5f;
-        double miDecimal = 22.5;
-        String miTexto = "Hola soy un texto en Java";
-        char miCaracter = 'a';*/
-
-        /*
-        Scanner scanner = new Scanner(System.in);
-
-        String nombre;
-        System.out.println("Ingrese su nombre completo");
-        nombre = scanner.nextLine();
-        int edad;
-        System.out.println("Ingrese su edad");
-        edad = scanner.nextInt();
-        double peso;
-        System.out.println("Ingrese su peso");
-        peso = scanner.nextDouble();
-        double altura;
-        System.out.println("Ingrese su altura");
-        altura = scanner.nextDouble();
-        double imc = peso / (altura * altura);
-
-        System.out.println("Su IMC es: " + imc);
-        System.out.println("Su Nombre  es: " + nombre);
-        System.out.println("Su edad es: " + edad);
-        System.out.println("Su peso es:" + peso);
-        System.out.println("Su altura es:" + altura);*/
-
-        //Desarrollar un programa que pida nombre apellido domicilio cuanto cotizo en 2022 y cunto impuesto va a pagar declarar constante de isr de 12%
-
-      /*  final double ISR = 0.12;
-
-        String nombre;
-        String apellido;
-        String domi;
-        double coti;
-
+        //Desarrolaun programa que pregunte al usuario 3 numeros y decirle cual es mayor
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese nombre: ");
-        nombre = scanner.nextLine();
 
-        System.out.println("Ingrese apellidos: ");
-        apellido = scanner.nextLine();
+        int n1,n2,n3,n4;
 
-        System.out.println("Ingresar Domicilio: ");
-        domi = scanner.nextLine();
+        System.out.println("Ingrese numero 1: ");
+        n1 = scanner.nextInt();
+        System.out.println("Ingrese numero 2: ");
+        n2 = scanner.nextInt();
+        System.out.println("Ingrese numero 3: ");
+        n3 = scanner.nextInt();
+        System.out.println("Ingrese numero 4: ");
+        n4 = scanner.nextInt();
 
-        System.out.println("Ingrese la cantidad a Cotizar: ");
-        coti = scanner.nextDouble();
-
-        double result = coti * ISR;
-
-        System.out.println("Su nombre es: " + nombre);
-        System.out.println("Sus apellidos son: " + apellido);
-        System.out.println("Su domicilio es: " + domi);
-        System.out.println("El ISR es de: "+ result);
-
-       */
-
-        /*
-        1 Nombre Apellido domicilio precio de su carro
-        2-. Calcular elimpusesto de tenecia = precio del carro  1.3 %
-         */
-
-       /*
-        Scanner scanner = new Scanner(System.in);
-
-        final double  tenencia= 0.013 ;
-
-         String nombre;
-         String apelli;
-         String domi;
-         double precar;
-
-        System.out.println("Ingrese su nombre: ");
-        nombre = scanner.nextLine();
-
-        System.out.println("Ingrese sus Apellidos:");
-        apelli = scanner.nextLine();
-
-        System.out.println("ingrese domicilio: ");
-        domi = scanner.nextLine();
-
-        System.out.println("ingrese el precio de su carro: ");
-        precar = scanner.nextDouble();
-
-         double resulta = precar *  tenencia;
-
-        System.out.println("El nombre es: "+ nombre);
-        System.out.println("Sus apellidos son: "+apelli);
-        System.out.println("Su domicilio es: "+ domi);
-        System.out.println("Su tenencia es:"+ resulta);
-        */
-
-        int a = 20;
-        int b = 5;
-        int c = 3;
-
-        if(a>b){
-            System.out.println("a es mayor que b");
-        }else{
-            System.out.println("a no es mayor que b");
+        if (n1 > n2 && n1 > n3 && n1 > n4){
+            System.out.println("El numero mas grande es: "+ n1);
+        } else if (n2 > n1 && n2 > n3 && n2 > n4) {
+            System.out.println("El numero mas grande es: " + n2);
+        }else if(n3> n1 && n3 > n2 && n3 > n4){
+            System.out.println("El numero mas grande es: " + n3);
+        }else if(n4> n1 && n4 > n2 && n4 > n3){
+            System.out.println("El numero mas grande es: " + n4);
+        }
+        else{
+            System.out.println("Los numeros son iguales");
         }
 
 
---
+++ Python/Ejercicio 2.py
@@ -0,0 +1,12 @@
+num = input("Ingrese un numero: ") 
+num = int(num)
+
+b = 100
+c = 1000000
+
+print (num)
+if num >= b and num <= 1000000:
+     print("El valor de a entra en el campo establecido")
+else:
+    print("El valor de a esta fuera delos campos establecidos")
+ 
