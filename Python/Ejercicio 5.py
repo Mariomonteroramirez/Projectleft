1755ae3b5b637bf60f9adbcb0364175b5a365ebf
Mariomonteroramirez <maro2000jv@gmail.com>
2023-03-03 11:14:43 -0600

    Ejercicio 5 y 6

---
+++ Python/Ejercicio 5.py
@@ -0,0 +1,12 @@
+
+divisor = 2
+
+basetriangulo = input("Ingresa la base de el triangulo :")
+basetriangulo = float(basetriangulo)
+
+alturatriangulo = input("Ingresa la altura de el triangulo :")
+alturatriangulo = float(alturatriangulo)
+
+result = basetriangulo * alturatriangulo / divisor
+
+print("El area deun triangulo es : ",result)
---
+++ Python/Ejercicio 6.py
@@ -0,0 +1,11 @@
+pi = 3.1416
+
+radiocirculo = input("Ingresa el radio de el circulo: ")
+radiocirculo = float(radiocirculo)
+
+radiocuadrado = radiocirculo * radiocirculo
+
+resulta = pi * radiocuadrado
+
+print("el area de el circulo es: ",resulta)
+
